4fb2f1bf1cf1993ab1f0eb5d55fb62f47c5ee68c
alex-wdmg <wdmg.com.ua@gmail.com>
2019-03-29 01:48:23 +0200

    Added base migrations

---
+++ migrations/m28032019_235559_messages.php
@@ -0,0 +1,82 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m28032019_235559_messages
+ */
+class m28032019_235559_messages extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%messages}}', [
+            'id'=> $this->bigPrimaryKey(20),
+            'sender_id' => $this->integer()->notNull(),
+            'receiver_id' => $this->integer()->notNull(),
+
+            'subject' => $this->string(64)->null(),
+            'message'=> $this->text()->notNull(),
+
+            'status' => $this->tinyInteger(1)->null()->defaultValue(0), // 0 - draft, 1 - sent, 2 - delivered, 3 - read, 4 - deleted
+            'session' => $this->string(32)->notNull(),
+
+            'created_at' => $this->dateTime()->defaultExpression('CURRENT_TIMESTAMP'),
+            'updated_at' => $this->datetime()->defaultExpression('CURRENT_TIMESTAMP'),
+
+        ], $tableOptions);
+
+        $this->createIndex('idx_messages_user','{{%messages}}', ['sender_id', 'receiver_id'],false);
+        $this->createIndex('idx_messages_text','{{%messages}}', ['subject', 'message'],false);
+        $this->createIndex('idx_messages_status','{{%messages}}', ['status'],false);
+        $this->createIndex('idx_messages_session','{{%messages}}', ['session'],false);
+
+        // If exist module `Users` set foreign key `user_id` to `users.id`
+        if(class_exists('\wdmg\users\models\Users') && isset(Yii::$app->modules['users'])) {
+            $userTable = \wdmg\users\models\Users::tableName();
+            $this->addForeignKey(
+                'fk_messages_to_users',
+                '{{%messages}}',
+                'sender_id, receiver_id',
+                $userTable,
+                'id',
+                'NO ACTION',
+                'CASCADE'
+            );
+        }
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropIndex('idx_messages_user', '{{%messages}}');
+        $this->dropIndex('idx_messages_text', '{{%messages}}');
+        $this->dropIndex('idx_messages_status', '{{%messages}}');
+        $this->dropIndex('idx_messages_session', '{{%messages}}');
+
+        if(class_exists('\wdmg\users\models\Users') && isset(Yii::$app->modules['users'])) {
+            $userTable = \wdmg\users\models\Users::tableName();
+            if (!(Yii::$app->db->getTableSchema($userTable, true) === null)) {
+                $this->dropForeignKey(
+                    'fk_messages_to_users',
+                    '{{%messages}}'
+                );
+            }
+        }
+
+        $this->truncateTable('{{%messages}}');
+        $this->dropTable('{{%messages}}');
+    }
+
+}
