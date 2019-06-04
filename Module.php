47b1f76bb39283e3c408ba9fdaaae169732c5ddf
alex-wdmg <wdmg.com.ua@gmail.com>
2019-06-05 02:17:09 +0300

    Module transferred to base module interface. Update Yii2 version.

--- Module.php
+++ Module.php
@@ -6,7 +6,7 @@ namespace wdmg\messages;
  * Yii2 Private messages
  *
  * @category        Module
- * @version         0.0.4
+ * @version         0.0.5
  * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
  * @link            https://github.com/wdmg/yii2-messages
  * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
@@ -15,11 +15,12 @@ namespace wdmg\messages;
  */
 
 use Yii;
+use wdmg\base\BaseModule;
 
 /**
- * messages module definition class
+ * Messages module definition class
  */
-class Module extends \yii\base\Module
+class Module extends BaseModule
 {
     /**
      * {@inheritdoc}
@@ -32,11 +33,6 @@ class Module extends \yii\base\Module
     public $defaultRoute = "messages/index";
 
     /**
-     * @var string the prefix for routing of module
-     */
-    public $routePrefix = "admin";
-
-    /**
      * @var string, the name of module
      */
     public $name = "Messages";
@@ -47,118 +43,17 @@ class Module extends \yii\base\Module
     public $description = "Private messaging system";
 
     /**
-     * @var string the vendor name of module
-     */
-    private $vendor = "wdmg";
-
-    /**
      * @var string the module version
      */
-    private $version = "0.0.4";
+    private $version = "0.0.5";
 
     /**
      * @var integer, priority of initialization
      */
     private $priority = 8;
 
-    /**
-     * @var array of strings missing translations
-     */
-    public $missingTranslation;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function init()
+    public function bootstrap($app)
     {
-        parent::init();
-
-        // Set controller namespace for console commands
-        if (Yii::$app instanceof \yii\console\Application)
-            $this->controllerNamespace = 'wdmg\messages\commands';
-
-        // Set current version of module
-        $this->setVersion($this->version);
-
-        // Register translations
-        $this->registerTranslations();
-
-        // Normalize route prefix
-        $this->routePrefixNormalize();
-    }
-
-    /**
-     * Return module vendor
-     * @var string of current module vendor
-     */
-    public function getVendor() {
-        return $this->vendor;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function afterAction($action, $result)
-    {
-
-        // Log to debuf console missing translations
-        if (is_array($this->missingTranslation) && YII_ENV == 'dev')
-            Yii::warning('Missing translations: ' . var_export($this->missingTranslation, true), 'i18n');
-
-        $result = parent::afterAction($action, $result);
-        return $result;
-
-    }
-
-    // Registers translations for the module
-    public function registerTranslations()
-    {
-        Yii::$app->i18n->translations['app/modules/messages'] = [
-            'class' => 'yii\i18n\PhpMessageSource',
-            'sourceLanguage' => 'en-US',
-            'basePath' => '@vendor/wdmg/yii2-messages/messages',
-            'on missingTranslation' => function($event) {
-
-                if (YII_ENV == 'dev')
-                    $this->missingTranslation[] = $event->message;
-
-            },
-        ];
-
-        // Name and description translation of module
-        $this->name = Yii::t('app/modules/messages', $this->name);
-        $this->description = Yii::t('app/modules/messages', $this->description);
-    }
-
-    public static function t($category, $message, $params = [], $language = null)
-    {
-        return Yii::t('app/modules/messages' . $category, $message, $params, $language);
-    }
-
-    /**
-     * Normalize route prefix
-     * @return string of current route prefix
-     */
-    public function routePrefixNormalize()
-    {
-        if(!empty($this->routePrefix)) {
-            $this->routePrefix = str_replace('/', '', $this->routePrefix);
-            $this->routePrefix = '/'.$this->routePrefix;
-            $this->routePrefix = str_replace('//', '/', $this->routePrefix);
-        }
-        return $this->routePrefix;
-    }
-
-    /**
-     * Build dashboard navigation items for NavBar
-     * @return array of current module nav items
-     */
-    public function dashboardNavItems()
-    {
-        return [
-            'label' => $this->name,
-            'url' => [$this->routePrefix . '/messages/'],
-            'active' => in_array(\Yii::$app->controller->module->id, ['messages'])
-        ];
+        parent::bootstrap($app);
     }
 }
